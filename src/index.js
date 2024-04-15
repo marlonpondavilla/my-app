cf4e63dada553dbe4d927657198705d3ad21b214
Marlonlon <marlonpondavlla.com>
2024-04-15 22:41:27 +0800

    I've learned: Components, JSX, props, naming convention in reactjs, map function, creating components and passing data

--- src/index.js
+++ src/index.js
@@ -1,17 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81tVBUv75vL._AC_UL300_SR300,200_.jpg",
+    title: "Just for the Summer Paperback",
+    author: "Abby Jimenez",
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/713-J3tw46L._AC_UL600_SR600,400_.jpg",
+    title: "Dog Man: The Scarlet Shedder: A Graphic Novel (Dog Man #12)",
+    author: "Dav Pilkey ",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81YkqyaFVEL._AC_UL600_SR600,400_.jpg",
+    title:
+      "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+    author: "James Webb ",
+  },
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <EventExamples />
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
+    </section>
+  );
+};
+
+const EventExamples = () => {
+  const handleFOrmInput = (e) =>{
+    console.log(`input name: ${e.target.name}`);
+    console.log(`input value: ${e.target.value}`);
+  }
+
+  const handleButtonClick = (e) => {
+    alert("handleButtonClick Fired");
+  };
+
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    console.log("Form submitted!");
+  };
+  return (
+    <section>
+      <form onSubmit={handleFormSubmission}>
+        <h2>Typical form</h2>
+        <input type="text" name="example" onChange={handleFOrmInput} style={{ margin: "1rem 0" }} />
+      </form>
+      <button onClick={handleFormSubmission}>click me!</button>
+    </section>
+  );
+};
+
+const Book = (props) => {
+  const { img, title, author } = props;
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+
+      {/* {console.log("Hello world")} */}
+    </article>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
