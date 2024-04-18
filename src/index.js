350920d9198d5000585c944e8ef9bcdd2108f3fc
Marlonlon <marlonpondavlla.com>
2024-04-18 12:38:22 +0800

    new book

--- src/index.js
+++ src/index.js
@@ -11,18 +11,17 @@ const books = [
   },
   {
     id: 2,
-    img: "https://images-na.ssl-images-amazon.com/images/I/713-J3tw46L._AC_UL600_SR600,400_.jpg",
-    title: "Dog Man: The Scarlet Shedder: A Graphic Novel (Dog Man #12)",
-    author: "Dav Pilkey ",
+    img: "https://images-na.ssl-images-amazon.com/images/I/611X8GI7hpL._AC_UL600_SR600,400_.jpg",
+    title: "The 48 Laws of Power",
+    author: "Robert Greene",
   },
   {
     id: 3,
     img: "https://images-na.ssl-images-amazon.com/images/I/81YkqyaFVEL._AC_UL600_SR600,400_.jpg",
     title:
       "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
-    author: "James Webb ",
+    author: "James Clear ",
   },
-  
 ];
 
 const BookList = () => {
